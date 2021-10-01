c52921228a7a44556a8cb1c31076f06d0c048ba5
Obbie <obipras@gmail.com>
2021-10-01 15:41:52 +0700

    Add SaveLoad(Local)

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start ()
+    {
+        UserDataManager.Load ();
+        SceneManager.LoadScene (1);
+    }
+}
---
+++ Assets/Scripts/UserProgressData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class UserProgressData
+{
+    public double Gold = 0;
+    public List<int> ResourcesLevels = new List<int> ();
+}
